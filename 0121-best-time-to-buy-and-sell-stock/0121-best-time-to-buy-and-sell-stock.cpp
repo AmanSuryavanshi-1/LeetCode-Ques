0ae28af7965530c6d50ae093ac44a1deff284523
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-01-05 12:20:43 +0530

    Time: 186 ms (51.80%), Space: 93.3 MB (53.83%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-      int j = INT_MAX;; // least price found in array
-        int k= 0; //profit if sold today 
-        int mp= 0; //max profit that can be acheived
-        
+    
+        int maxProfit = 0;
+        int minSoFar = prices[0];
         for(int i=0; i<prices.size(); i++){
-            if(prices[i]<j){ // if we found new buy value which is more smaller then previous one
-                j=prices[i]; // updating least price
-            }
-            k= prices[i]-j; // calculating profit if sold today, price at particular day - least price
-            
-            if(mp<k){  // finding max profit acheived by comparing k of each day
-                mp=k; // update maxl profit
-            }
+            minSoFar=min(minSoFar,prices[i]);
+            int profit = prices[i]-minSoFar;  
+            maxProfit= max(maxProfit,profit);
         }
-         return mp;
-        
-        // Time O(n)
-        // space O(1)
+        return maxProfit;
     }
+    
+    
+//       int j = INT_MAX;; // least price found in array
+//         int k= 0; //profit if sold today 
+//         int mp= 0; //max profit that can be acheived
+        
+//         for(int i=0; i<prices.size(); i++){
+//             if(prices[i]<j){ // if we found new buy value which is more smaller then previous one
+//                 j=prices[i]; // updating least price
+//             }
+//             k= prices[i]-j; // calculating profit if sold today, price at particular day - least price
+            
+//             if(mp<k){  // finding max profit acheived by comparing k of each day
+//                 mp=k; // update maxl profit
+//             }
+//         }
+//          return mp;
+        
+//         // Time O(n)
+//         // space O(1)
+//     }
+        
+        // another way to write above code
+        
+       
 };
