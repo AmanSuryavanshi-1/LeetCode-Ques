b6457d865b93729041d3c372b6e38879b562514d
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-01-07 01:27:07 +0530

    Time: 65 ms (85.00%), Space: 33.6 MB (81.80%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -1,11 +1,26 @@
-// #Approach 1
-// Time Complexcity - O(N*N) <=Give You TLE
-// Space Complexcity - O(1)
-
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int>ans; //vector for storing dulpicate 
+                
+// #Approach 3 -Best
+// Time Complexcity - O(N)
+// Space Complexcity - O(1)
+       for(int i=0; i<nums.size(); i++){
+           // take the absolute value of nums[i] bcz index are not -ve
+           if(nums[abs(nums[i])-1]>0)//reaching here for the first time
+           {
+               nums[abs(nums[i])-1] = -1*nums[abs(nums[i])-1]; // multiply it by -1
+           }
+           else 
+               ans.push_back(abs(nums[i]));// if the element at the index abs(nums[i])-1 is not positive, add abs(nums[i]) to the ans vector
+        }
+        return ans;
+        
+
+// #Approach 1
+// Time Complexcity - O(N*N) <=Give You TLE
+// Space Complexcity - O(1)
     //     for(int i=0;i<nums.size();i++){
     //         for(int j=i+1;j<nums.size();j++){
     //             if(nums[i]!=nums[j])continue;
@@ -17,15 +32,18 @@ public:
     //     }
     //     return ans;
     
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++){ 
-            int n = nums[i]; // storing value in n from array 
-            mp[n]++; // storing n in the map
-        }
-        for(auto j:mp) // traversing through map
-            if(j.second==2){
-                ans.push_back(j.first);
-            }
-        return ans;
-    }
+// #Approach 2
+// Time Complexcity - O(N)
+// Space Complexcity - O(N)
+    //     unordered_map<int,int> mp;
+    //     for(int i=0;i<nums.size();i++){  
+    //         mp[nums[i]]++;  // storing value in n from array
+    //     }
+    //     for(auto j:mp) // traversing through map
+    //         if(j.second==2){
+    //             ans.push_back(j.first);
+    //         }
+    //     return ans;
+    
+    }    
 };
