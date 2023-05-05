c708a343d24fc4b4709ade211391901c1a9fc20f
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-05-05 12:41:54 +0530

    Time: 102 ms (47.18%), Space: 41.3 MB (96.74%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,33 +1,50 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
- unordered_map<int,int> mp;
-        int sum=0,ans=0;
-        mp[sum] = 1;
-        for(auto it:nums){
-            sum += it;
-            int find = sum - k;
-            if(mp.find(find) != mp.end()){
-                ans += mp[find];
-            }
-            mp[sum]++;
-        }
-        return ans;
-    }
-};
-
-//  class Solution {
-// public:
-//     int subarraySum(vector<int>& nums, int k) {
-//         int sum =0;
-//         int result = 0;
-//         for (int i=0; i<=nums.size();i++){
-//             sum+=nums[i];
-//             if(sum-k==0 )
+       
+        
+// Time Complexity --> O(n ^ 2)
+// Space Complexity --> O(1) 
+        
+//         int count = 0;
+//         for (int i=0; i<nums.size();i++){
+//             int sum = nums[i];
+//             if(sum == k) // if element itself equal to k, then also increment count
+//                 count++;
+            
+//             for (int j= i+1; j<nums.size();j++){
+//             sum+=nums[j];
+//             if(sum==k )
 //             {
-//                 result++;
+//                 count++;
 //             }
 //  }
-// return result;
+//             }
+// return count;
 //     }
 // };
+        
+              
+// Time Complexity --> O(n)
+// Space Complexity --> O(n)   //hashmap is used
+         unordered_map<int,int> m;
+        int sum=0,count=0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            if(sum == k) count++;
+            
+            if(m.find(sum - k) != m.end()){
+                count += m[sum - k];
+            }
+            
+            if(m.find(sum)!=m.end()){
+                m[sum]++;
+            }
+            else{
+            m[sum]++; // put sum into our map
+            }
+            }
+        return count;
+    }
+};
