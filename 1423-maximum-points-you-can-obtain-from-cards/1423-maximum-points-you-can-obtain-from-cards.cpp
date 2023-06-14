4fdb3547deeb316b714a8c6bba21861ae78beedf
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-06-14 22:52:14 +0530

    Time: 70 ms (50.34%), Space: 42.3 MB (63.16%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -9,10 +9,10 @@ public:
         for(int i =k-1;i>=0;i--){
             //We remove the last visited element 
              sum-=cardPoints[i]; // excluding 
+            
             // and add the non-visited element from the last
             sum+=cardPoints[cardPoints.size()-k+i]; // including
-            maxsum= max(sum,maxsum);
-            
+            maxsum= max(sum,maxsum);     
         }
         
         return maxsum;
