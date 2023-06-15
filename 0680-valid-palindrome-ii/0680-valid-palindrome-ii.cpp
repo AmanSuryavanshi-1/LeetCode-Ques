bdf3f663fa153b876cea51398d33aaab030b63e1
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-06-15 19:49:32 +0530

    Time: 44 ms (92.30%), Space: 24 MB (35.99%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,42 @@
+// Time complexity - O(N)
+// Space complexity- O(1)
+class Solution {
+public:
+    bool isPal(string s, int l, int r)
+    {
+        while(l < r)
+        {
+            if(s[l] != s[r])
+                return false;
+            else{
+            l++;
+            r--;
+            }
+        }
+        
+        return true;
+    }
+public:
+    bool validPalindrome(string s) {
+        int st = 0, en = s.length()-1;
+        // st= start, en=end
+        // we're going to check first and last
+        // character in pairs
+        while(st < en)
+        {
+            // if there's mismatch, we're going to 
+            // ignore one character 
+            if(s[st] != s[en])
+            {
+                // we have two options
+                // 1. we can ignore first character. 
+                // 2. we can ignore second character.
+                // ignoring one character, if the rest  is a palindrom, it's our answer
+                return isPal(s, st+1, en) || isPal(s, st, en-1);
+            }
+            st++;
+            en--;
+        }
+        return 1;
+    }
+};
