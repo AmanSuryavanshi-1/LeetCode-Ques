ad70a12651d6a75b77a0f125059e113715aaecd7
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-08-22 13:40:18 +0530

    Time: 2 ms (72.22%), Space: 6 MB (55.58%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+// Time Complexity - O(N)
+// Space Complexity - O(1)
+        if(n < 2) 
+            return n;
+        int a=0, b=1, sum=0;
+        
+        for(int i=2; i<=n;i++){
+            sum = a + b;
+            a=b;
+            b=sum;
+        }
+        return sum;
+    }
+};
+
