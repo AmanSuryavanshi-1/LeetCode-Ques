2eebe033a14913c81350bfd400e695a14eef28c1
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-08-27 20:39:12 +0530

    Time: 23 ms (46.66%), Space: 10.9 MB (23.10%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+
+    // Sliding window, expand if unique, contract if duplicate
+    // Time: O(n)
+    // Space: O(n)
         unordered_set<char> mySet;
         int left = 0;
         int res = 0;
 
         for (int right = 0; right < s.length(); right++) {
-            while (mySet.find(s[right]) != mySet.end()) {
+            while (mySet.find(s[right]) != mySet.end()) { 
+// a while loop that continues to execute as long as the element s[right] is found in the std::set, i.e., as long as mySet.find(s[right]) doesn't return an iterator pointing to the "end" of the set.
                 mySet.erase(s[left]);
                 left++;
             }
             mySet.insert(s[right]);
-            res = max(res, right - left + 1);
+            res = max(res, right - left + 1);   // we are storing max number of unique substr in the process
         }
         return res;
     }
-};
+}; 
