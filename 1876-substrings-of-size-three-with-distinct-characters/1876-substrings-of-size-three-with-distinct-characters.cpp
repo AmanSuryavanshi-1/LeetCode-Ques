3be3629f7f0d400f0e4501df542f71ccc7040bf0
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-08-31 14:13:25 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (60.93%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -4,12 +4,16 @@ public:
 //  Time Complexity: O(n), where 'n' is the length of the input string s.
 // Space Complexity: O(1)
         if(s.length() < 3) return 0;
-        int res=0;
-        for(int i=0; i<s.length()-2; i++){
-            if(s[i]!=s[i+1] && s[i+1]!=s[i+2] && s[i+2]!=s[i]){
-                res++;
-            }
-        }
+    //     int res=0;
+    //     for(int i=0; i<s.length()-2; i++){
+    //         if(s[i]!=s[i+1] && s[i+1]!=s[i+2] && s[i+2]!=s[i]){
+    //             res++;
+    //         }
+    //     }
+    //     return res;
+    int res=0;
+    for(int i=1;i<s.size()-1;i++)
+            if(s[i]!=s[i-1] && s[i]!=s[i+1] && s[i-1]!=s[i+1])res++;   
         return res;
     }
 };
