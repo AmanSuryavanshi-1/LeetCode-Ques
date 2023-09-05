99bc9f122a19aa7879a26c40ed28fabfba5a27d7
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-09-05 13:02:40 +0530

    Time: 3 ms (88.52%), Space: 7.8 MB (71.95%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int res=0;
+        for(int i=0; i<accounts.size();i++){
+            int sum=0;
+            for(int j=0; j<accounts[i].size(); j++){
+            sum += accounts[i][j]; // ij 01 02 03 (next iteration) = 11 12 13
+                }
+            res = max(res,sum);
+        }
+        
+    return res;
+    }
+};
+//Time Complexity: O(m * n)
+//Space Complexity: O(1)
