ba632f12df80248cfeaf7eab409d29e12cbe7194
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-09-28 07:30:50 +0530

    Time: 90 ms (22.85%), Space: 68 MB (15.43%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    
-//     Time complexity = O(n)
-//     Space = O(1)
-    // KADEN's Algo
-        
+// Time complexity = O(n)
+// Space = O(1)
+// KADEN's Algo
     int maxSubArray(vector<int>& nums) {
+     int maxSum = INT_MIN;
+     int curSum = 0;
         
-         int maxSum = INT_MIN; // for maximum value
-        int curSum = 0; // for larger sum
-        
-        for(int i = 0; i< nums.size();i++){
-            
-            curSum += nums[i]; 
-            if (curSum > maxSum){
-                maxSum = curSum;
-            }
-            if (curSum < 0){
-                // if sum become -ve then we should discard till that point and fresh start to find max value
-                curSum = 0;
-            }
+        for(int i=0; i<nums.size(); i++){
+            curSum+=nums[i];
+            if(curSum > maxSum) maxSum = curSum;
+            if(curSum < 0) curSum=0;
+              // if sum become -ve then we should discard till that point and fresh start to find max value
         }
         return maxSum;
     }
