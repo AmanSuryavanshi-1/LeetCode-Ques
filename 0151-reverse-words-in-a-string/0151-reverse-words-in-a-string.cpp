cc30ae98eaec370de0fce2de4f0aea4eb7265d71
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-09-30 12:01:20 +0530

    Time: 2 ms (88.02%), Space: 21.1 MB (15.22%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,5 +1,8 @@
 class Solution {
-public:
+public:    
+// Time complexity: O(n)
+// Space complexity: O(n)
+    
     string reverseWords(string s) {
     //        i is for char
     // j is looking for space
@@ -9,15 +12,17 @@ public:
         string result;
 
         while(i<n){
-            while(i<n && s[i] == ' ') i++;
+            while(i<n && s[i] == ' ') i++; // Skip spaces until a non-space character is found.
             if(i>=n) break;
-            int j = i+1;
-            while(j<n && s[j] != ' ') j++;
+            int j = i+1; // Initialize a pointer to look for the end of the current word.
+            while(j<n && s[j] != ' ') j++; // Continue until a space or the end of the string is found.
             string sub = s.substr(i, j-i);
-            if(result.length() == 0) result = sub;
-            else result = sub+ " " + result; // adding new substring in front of previous substring
-            i=j+1;
+            if(result.length() == 0) result = sub; 
+            else result = sub+ " " + result; // adding current substring in front of previous substring
+            i=j+1; 
         }
-        return result;
+        return result; 
+        
+
     }
 };
