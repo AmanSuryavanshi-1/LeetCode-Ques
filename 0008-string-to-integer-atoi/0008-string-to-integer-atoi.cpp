2ff231d7eed0054c7ca9238a33ce1d4b2b30848b
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-10-01 10:50:13 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (24.76%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,44 +1,36 @@
 class Solution {
 public:
     int myAtoi(string s) {
-//         int ans=0;
-//         int i;
-//         while(i<s.length()){
-//             // if(s[i]>='a' && s[i]<='z' || s[i]==' ') i++;
-//             if(isdigit(s[i])) ans+='s[i]';
-//             else if(s[i]=='-') ans+='-';
-//             else 
-//                 i++;
-                
-//         }
-//         return (int)ans;
+// Time complexity: O(N)
+// Space complexity: O(1)
+        
         int i=0;
         int n=s.size();
-        double ans=0;
+        double ans=0;     //using double instead of long bcz range of double is more
         
-        while(i<n && s[i]==' ') i++;
+        while(i<n && s[i]==' ') i++;  // skipping space characters at the beginning
         
-        int positive =0, negative=0;
+        int positive =0, negative=0; 
         if(i<n && s[i]=='+') {
-            positive++;
+            positive++;    // number of positive signs at the start in string
             i++;
         }
         if(i<n && s[i]=='-'){
-            negative++;
+            negative++;    // number of negative signs at the start in string
             i++;
         }
        
         
 
         while(i<n && s[i]>='0' && s[i]<='9') {
-            ans = ans*10 + (s[i]-'0');
+            ans = ans*10 + (s[i]-'0'); // (s[i] - '0') is converting character to integer
             i++;
         }
-         if(negative>0) {ans = -ans;}
-        if(positive>0 && negative>0) return 0;
+         if(negative>0) {ans = -ans;}  // if negative sign exist
+        if(positive>0 && negative>0) return 0;  // if both +ve and -ve sign exist, Example: +-12 then ans =0
         
-        if(ans>INT_MAX) return INT_MAX;
-        if(ans<INT_MIN) return INT_MIN;
+        if(ans>INT_MAX) return INT_MAX; // if ans > 2^31 - 1
+        if(ans<INT_MIN) return INT_MIN; // if ans < 2^31
 
         return static_cast<int>(ans);
     }
