6e33bfb72183257581d34023f1dd39989d7f66bb
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-10-02 12:27:54 +0530

    Time: 42 ms (84.79%), Space: 48.8 MB (30.57%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        // Space Complexity: O(1)
-        // Time Complexity: O(n)
+// Space Complexity: O(1)
+// Time Complexity: O(n)
+        
         int n = nums.size();
         int goal = n - 1;
         for (int i = n - 2; i >= 0; i--) { 
