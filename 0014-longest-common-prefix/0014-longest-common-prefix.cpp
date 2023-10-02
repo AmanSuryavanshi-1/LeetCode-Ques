2e7e3386a05368be793224a5c0ed04d0c2ba3a37
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-10-02 08:39:39 +0530

    Time: 6 ms (34.32%), Space: 9.9 MB (11.24%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        
+// Time complexity: O(m * n * log(n)),'m' is the length of the longest string in 'strs'. and nlogn for sort
+//Space complexity: O(1)
+        
        int n = strs.size();
         if(n==0) return "";
         
