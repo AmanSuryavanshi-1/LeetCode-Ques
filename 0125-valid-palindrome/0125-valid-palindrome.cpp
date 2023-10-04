de9988b19948a07543e5c092f1c280467ec183af
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-10-04 18:36:50 +0530

    Time: 2 ms (92.87%), Space: 7.8 MB (17.95%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,31 +4,47 @@ public:
 // Time complexity: O(n)
 // Space complexity: O(n)  string 't' can be up to the same length as the input string 's' 
         
-        string t;
-        for(const auto& a:s)
-        {
-            if(a>='A' && a<='Z')
-            {
-                char lowercase = tolower(a);
-                t.push_back(lowercase);
-                // t.pk(a-'A' + 'a');
-            }
-            else if(a>='a' && a<='z')
-            {
-                t.push_back(a);
-            }
-            else if(a>='0' && a<='9')
-            {
-                t.push_back(a);
-            }
+//         string t;
+//         for(const auto& a:s)
+//         {
+//             if(a>='A' && a<='Z')
+//             {
+//                 char lowercase = tolower(a);
+//                 t.push_back(lowercase);
+//                 // t.pk(a-'A' + 'a');
+//             }
+//             else if(a>='a' && a<='z')
+//             {
+//                 t.push_back(a);
+//             }
+//             else if(a>='0' && a<='9')
+//             {
+//                 t.push_back(a);
+//             }
+//         }
+        
+//         string temp=t;
+        
+//         reverse(t.begin(),t.end());
+//         if (t==temp) return true;     
+
+//         return false;
+        
+// Time complexity: O(n)
+// Space complexity: O(1)
+        
+        int start= 0;
+        int end = s.length()-1;
+        
+        while(start<=end){
+            // Skip non-alphanumeric characters at the beginning of the string.
+            if(!isalnum(s[start])){ start++; continue;} 
+            if(!isalnum(s[end])) {end--; continue;}
+            if(tolower(s[start])!=tolower(s[end])) return false;
+            else
+                start++;
+                end--;
         }
-        
-        string temp=t;
-        
-        reverse(t.begin(),t.end());
-        if (t==temp) return 1;     
-
-        return 0;
-    }   
-
+        return true;
+    }
 };
