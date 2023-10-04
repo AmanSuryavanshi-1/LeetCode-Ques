dff721cf0efb7307217e30da4fc105c4f687c0bd
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-10-04 12:03:22 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (7.35%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -18,16 +18,10 @@ public:
         int count =0;
         for (int i = 0; i < nums.size(); i++) {
         if (mp.find(nums[i]) != mp.end()) {
-            // Check if 'num' is found in the 'mp' (unordered map).
-
             count += mp[nums[i]];
-            // If 'nums[i]' is found, increment 'count' by the value associated with 'nums[i]' in the map.
             mp[nums[i]]++;
-            // Increment the count in the map for 'num' to indicate another occurrence.
         } else {
-            // If 'num' is not found in the map:
             mp[nums[i]] = 1;
-            // Initialize the count for 'num' in the map to 1, indicating that we've seen it once.
         }
     }
     
