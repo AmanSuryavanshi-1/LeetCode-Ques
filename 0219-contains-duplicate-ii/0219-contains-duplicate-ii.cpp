11484f0ae039aa7788bf6cf9564314d8beb9e2d4
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-10-09 13:31:16 +0530

    Time: 133 ms (49.27%), Space: 77.6 MB (47.69%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -12,10 +12,9 @@ public:
         int curr=nums[i];
 		
         if(mp.find(curr)!=mp.end()){                 //if curr_element is present in map
-            if(abs(i-mp[curr]) <= k) return true;    //then check abs(i-mp[curr]) <= k, return true;
-            // mp[curr]=i;                              //else update the index of curr_element;
+            if(abs(i-mp[curr]) <= k) return true;    //then check abs(i-mp[curr]) <= k, return true;}
         }
-        mp[curr]=i;                                  //else, push element and it's index into map
+            mp[curr]=i;                                  //else, push element and it's index into map
     }
     return false;
 }
