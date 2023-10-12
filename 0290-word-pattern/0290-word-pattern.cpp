000deb2500311c91c69b638964758cb1702c2860
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-10-12 13:42:20 +0530

    Time: 3 ms (43.02%), Space: 6.7 MB (31.77%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
+// Time complexity: O(n)
+// Space complexity: O(n), as the vector 'v' can store up to 'n' words from the string 's'. 
+//     The unordered_map 'm' and the set 'st' can also store up to 'n' unique words in the worst case.
+// Notes 29
+    
     bool wordPattern(string pattern, string s) {
-        vector<string> words;
-        istringstream iss(s);
-        string word;
-      
-        while (iss >> word) {
-            words.push_back(word);
+        vector<string> v;
+        string temp="";
+        int i=0;
+        while(i<s.size()){
+        if(s[i]==' '){
+            v.push_back(temp);
+            temp="";
         }
-      
-        if (words.size() != pattern.size())
-            return false;
-      
-        map<char, string> charToWord;
-        map<string, char> wordToChar;
-      
-        for (int i = 0; i < pattern.length(); i++) {
-            char c = pattern[i];
-            string w = words[i];
-          
-            if (charToWord.find(c) == charToWord.end() && wordToChar.find(w) == wordToChar.end()) {
-                charToWord[c] = w;
-                wordToChar[w] = c;
-            } else if (charToWord[c] != w || wordToChar[w] != c) {
-                return false;
+        else temp+=s[i];
+
+        i++;
+    }
+        v.push_back(temp);  // pushing last word
+        
+        if(pattern.size()!=v.size()) return false;
+        
+        unordered_map<char, string> m;
+        set<string> st;
+        for(int i=0; i<pattern.size(); i++){
+            if(m.find(pattern[i]) != m.end()){  //pattern[i] is in map
+                if(m[pattern[i]] != v[i]){      //pattern[i] is in map and its not equal to word [i]
+                    return false;
+                }
             }
-        }
-      
+            else{
+                if(st.count(v[i]) > 0) return false;  //if already present in stack
+            
+            m[pattern[i]] = v[i];
+            st.insert(v[i]);
+            }
+        } 
         return true;
     }
 };
