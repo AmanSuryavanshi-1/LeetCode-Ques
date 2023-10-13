7719c31c342ccf3a1b8dfb49ad23d5e5d22ed590
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-10-13 14:11:55 +0530

    Time: 118 ms (5.98%), Space: 6.2 MB (44.82%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
-    }
+// Time complexity: O(sqrt(x)), where 'x' is the input integer
+// Space complexity: O(1)
+        if(x==0) return 0;
+		else if (x==1) return 1;
+		for (int i=1;i<=x;i++)
+		{
+		   if(i*i==x)
+				return i;
+		   else if(i<=x/i &&  x/(i+1)<(i+1))
+				return (i);
+		}
+	  return -1;  
+	}
 };
