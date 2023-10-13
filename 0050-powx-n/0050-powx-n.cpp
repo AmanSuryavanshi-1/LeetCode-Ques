5897e87d86a4325522fb67843e7a5e1142907b4e
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-10-13 10:11:59 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (25.11%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        
+// Time: O(log2(n))
+// Space: O(1)
         if(n < 0) {
             x = 1 / x;
         } 
@@ -11,10 +12,10 @@ public:
         double pow = 1;
         
         while(num){ // equivalent to while(num != 0)
-            if(num % 2) { // equivalent to if((num & 1) != 0)
+            if(num % 2) {  //even
                 pow *= x;
             }
-            
+            //odd
             x *= x;
             num = num/2;
         }
