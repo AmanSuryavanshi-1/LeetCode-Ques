64dc2be3905b3888f13b488aaccf3d795805ed62
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2023-10-14 17:37:30 +0530

    Time: 15 ms (37.52%), Space: 8.7 MB (21.97%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int romanToInt(string s) {
+// Time Complexity: O(n)
+// Space Complexity: O(1)
         int ans=0;
         unordered_map <char,int> mp{
         {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
@@ -13,5 +15,3 @@ public:
         return ans;
     }
 };
-// Time Complexity: O(n)
-// Space Complexity: O(1)
