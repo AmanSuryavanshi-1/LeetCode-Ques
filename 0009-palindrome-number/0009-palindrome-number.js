18c09ce067fc95b72c8f898cca3155b00c2fca8c
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2024-06-24 06:27:35 +0530

    Time: 139 ms (42.29%), Space: 56.8 MB (72.71%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -4,19 +4,24 @@
  */
 var isPalindrome = (n) => {
 
+// Approach 1
+return n === +n.toString().split("").reverse().join("");
+// 123 => "123" => ["1","2","3"] => ["3","2","1"] => ( 3.2.1 -> 321 ) 
+// + n the start converts the string into a number if possible
+// console.log(+"10"); 10
+// console.log(+"a"); NAN
 
 // Optimized Way
-    if (n < 0) {
-            return false;
-        }
-        var remainder=0,Reverse=0;
-        const newN = n;
+    // if (n < 0) {
+    //         return false;
+    //     }
+    //     var remainder=0,Reverse=0;
+    //     const newN = n;
        
-        while(n!=0){
-            remainder=n%10;                     // 1    (12->2)        1
-            n=Math.floor(n/10);                             // 12      1           1
-            Reverse=Reverse*10+remainder;       // 1    (10+2=12)   (120+1)=121
-        }    
-        return (newN == Reverse);    
-        // if(newN == Reverse) return true;
+    //     while(n!=0){
+    //         remainder=n%10;                     // 1    (12->2)        1
+    //         n=Math.floor(n/10);                             // 12      1           1
+    //         Reverse=Reverse*10+remainder;       // 1    (10+2=12)   (120+1)=121
+    //     }    
+    //     return (newN == Reverse);    
     }
