013f97482d4fe98a0a5ff9001af8f43f3f578f08
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2024-06-25 07:33:33 +0530

    Time: 51 ms (76.3%), Space: 48.2 MB (92.65%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.js
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -7,13 +7,22 @@ var fib = function(n) {
 // Time Complexity - O(N)
 // Space Complexity - O(1)
 
-    if(n<2) return n;
+    // if(n<2) return n;
 
-    let a=0,b=1,sum=0;
-    for(let i=2; i<=n; i++){
-        sum = a+b;
-        a = b;
-        b = sum;
+    // let a=0,b=1,sum=0;
+    // for(let i=2; i<=n; i++){
+    //     sum = a+b;
+    //     a = b;
+    //     b = sum;
+    // }
+    // return sum;
+
+// Approach 2
+// Time Complexity - O(N)
+// Space Complexity - O(N)
+    const arr = [0,1];
+    for(let i =2; i<=n; i++){
+         arr.push(arr[i-1]+arr[i-2]);
     }
-    return sum;
+    return arr[n];  
 };
