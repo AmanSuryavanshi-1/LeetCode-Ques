0c08b1e02f76e39ee2bebee636b9729818754138
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2024-06-27 08:05:19 +0530

    Time: 72 ms (55.77%), Space: 51.1 MB (78.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,11 +4,31 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    s = s.split("").sort().join("");
-    t = t.split("").sort().join("");
+// Approach 1
+// Time Complexity: O(n log n)
+// Space Complexity: O(n)
 
-    // if(s==t) return true;
-    // return false;
-    // OR
-    return s==t;
+    // s = s.split("").sort().join("");
+    // t = t.split("").sort().join("");
+
+        // if(s==t) return true;
+        // return false;
+        // OR
+    // return s==t;
+
+// Approach 2
+if(s.length !== t.length) return false;
+
+let O1 = {};
+let O2 = {};
+
+for(let i=0; i<s.length; i++){
+    O1[s[i]] = (O1[s[i]] || 0) + 1;
+    O2[t[i]] = (O2[t[i]] || 0) + 1;
+}
+for(let key in O1){
+    if(O1[key] !== O2[key]) return false;
+}
+
+return true;
 };
