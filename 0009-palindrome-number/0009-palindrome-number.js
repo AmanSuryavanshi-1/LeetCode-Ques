d86bba9b7b39971e9e8d0d00e135ae62501851d9
Aman Suryavanshi <105555822+AmanSuryavanshi-1@users.noreply.github.com>
2024-06-27 07:12:48 +0530

    Time: 100 ms (99.29%), Space: 57.5 MB (51.56%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -7,11 +7,12 @@ var isPalindrome = (n) => {
 // Approach 1
 // Time Complexity: O(n)
 // Space Complexity: O(n)
+
 return n < 0 ? false : n === +n.toString().split("").reverse().join("");
 // 123 => "123" => ["1","2",3"] => ["3","2","1"] => ( 3.2.1 -> 321 ) 
 // + n the start converts the string into a number if possible
 
-// Optimized Way
+// Approach 2 ( Optimized Way )
 // Time Complexity: O(n)
 // Space Complexity: O(1)
 
